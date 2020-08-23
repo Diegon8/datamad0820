4c09f82eea824d59f23287d69e95a5d985c415a2
Diegon8 <diegolpr95@gmail.com>
2020-08-24 00:04:19 +0200

    GUERRA!

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,6 +1,7 @@
 
 # Soldier
 
+import random
 
 class Soldier:
      def __init__(self, vida, fuerza):
@@ -49,12 +50,38 @@ class Saxon(Soldier):
         
 
 # War
-'''
+
 class War:
     def __init__(self):
-
-
-
-
-
-    pass'''
+         self.vikingArmy = []
+         self.saxonArmy = []
+    def addViking(self, Viking):
+         self.vikingArmy.append(Viking)
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)        
+    def vikingAttack(self):
+        
+        self.saxon=random.choice(self.saxonArmy)
+        self.viking=random.choice(self.vikingArmy)
+        saxonleft=self.saxon.receiveDamage(self.viking.attack())
+        if self.saxon.health<=0:
+            self.saxonArmy.remove(self.saxon) 
+        return saxonleft  
+        
+    def saxonAttack(self):
+    
+        self.saxon=random.choice(self.saxonArmy)
+        self.viking=random.choice(self.vikingArmy)
+        vikingleft=self.viking.receiveDamage(self.saxon.attack())
+        if self.viking.health<=0:
+            self.vikingArmy.remove(self.viking)   
+        return vikingleft
+        
+    def showStatus(self):
+        if len(self.saxonArmy)==0:
+            return 'Vikings have won the war of the century!'
+        elif len(self.vikingArmy)==0:
+            return 'Saxons have fought for their lives and survive another day...'
+        else:
+            return 'Vikings and Saxons are still in the thick of battle.'
+         
