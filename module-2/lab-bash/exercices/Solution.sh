adacf42663525eab9e994daf5bfaf5ec36d1056b
Diegon8 <diegolpr95@gmail.com>
2020-09-01 17:19:47 +0200

    Bonus pending

--- module-2/lab-bash/exercices/Solution.sh
+++ module-2/lab-bash/exercices/Solution.sh
@@ -1,3 +1,39 @@
+#1
 echo 'Hello world'
+#2 Primero habría que ir con el comando cd hasta la carpeta exercice en la-bash
 mkdir new_dir
+#3
 rm -d new_dir
+#4 Primero tendría que ir con cd hasta la carpeta lorem donde está el archivo sed.txt
+cp sed.txt /home/diegon8/datamad0820/module-2/lab-bash/lorem-copy
+#5
+cp at.txt /home/diegon8/datamad0820/module-2/lab-bash/lorem-copy ; cp lorem.txt /home/diegon8/datamad0820/module-2/lab-bash/lorem-copy
+#6 
+cat sed.txt
+#7
+cat at.txt lorem.txt
+#8 Nos movemos a la carpeta lorem-copy con cd
+head -3 sed.txt
+#9 
+tail -3 sed.txt
+#10
+echo 'Homo homini lupus.' >> sed.txt
+#11
+tail -3 sed.txt
+#12
+sed -i 's/et/ET/g' at.txt
+#13
+whoami
+#14
+wc -l sed.txt
+#15
+ls -dq *lorem* | wc -l
+#16 Volvemos a la carpeta lorem con cd
+grep 'et' at.txt
+#17
+grep -c 'et' at.txt
+#18 volvemos a lorem-copy con cd
+grep -c 'et' at.txt lorem.txt sed.txt
+
+
+
